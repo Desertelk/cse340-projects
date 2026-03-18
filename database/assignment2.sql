e26d9815f4dad825929c93a1255417b1dbe98e97
Desertelk <cadenwatts111@gmail.com>
2026-03-17 21:56:59 -0500

    Assignement 2 progress

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -231,3 +231,40 @@ VALUES   (
     'White',
     5
   );
+
+INSERT INTO public.account (
+	account_firstname,
+	account_lastname,
+	account_email,
+	account_password
+) VALUES (
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
+
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+DELETE FROM account
+WHERE account_id = '1';
+
+SELECT *
+FROM inventory;
+
+UPDATE inventory
+SET inv_description = REPLACE (inv_description,'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+SELECT inv_make, inv_model, classification_name
+FROM inventory as i
+	INNER JOIN classification as c
+		ON i.classification_id = c.classification_id
+WHERE classification_name = 'Sport';
+
+UPDATE inventory
+SET inv_image = REPLACE (inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE (inv_thumbnail, '/images', 'images/vehicles');
+
