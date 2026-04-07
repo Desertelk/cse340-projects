a83c877f34fc8a1ac1136fdfda5c4945c82c80aa
Desertelk <cadenwatts111@gmail.com>
2026-04-06 21:39:32 -0500

    W05 Updating Data Progress 4/6/2026

--- controllers/invController.js
+++ controllers/invController.js
@@ -37,9 +37,11 @@ invCont.buildByInventoryId = async function (req, res, next){
 
 invCont.buildManagement = async function (req, res, next){
     let nav = await utilities.getNav()
+    const classificationSelect = await utilities.buildClassificationList()
     res.render("inventory/management", {
         title: "Inventory Management",
         nav,
+        classificationSelect,
     })
 }
 
