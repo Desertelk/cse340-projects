c865079f33817acf21f44c7dd23493a94a7aedb5
Desertelk <cadenwatts111@gmail.com>
2026-04-07 20:04:33 -0500

    W05 learning activities

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const router = new express.Router()
-const invController = require("../controllers/invController")
 const utilities = require("../utilities/index")
+const invCont = require("../controllers/invController")
 
-router.get("/type/:classificationId", invController.buildByClassificationId)
-router.get("/detail/:invId", invController.buildByInventoryId)
-router.get("/", utilities.handleErrors(invController.buildManagement))
-router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory))
-router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification))
+router.get("/type/:classificationId", invCont.buildByClassificationId)
+router.get("/detail/:invId", invCont.buildByInventoryId)
+router.get("/", utilities.handleErrors(invCont.buildManagement))
+router.get("/add-inventory", utilities.handleErrors(invCont.buildAddInventory))
+router.get("/add-classification", utilities.handleErrors(invCont.buildAddClassification))
+router.get("/getInventory/:classification_id", utilities.handleErrors(invCont.getInventoryJSON))
+router.get("/edit/:inv_id", utilities.handleErrors(invCont.buildEditInventory))
 
-router.post("/add-classification", utilities.handleErrors(invController.addClassification))
-router.post("/add-inventory", utilities.handleErrors(invController.addInventory))
+router.post("/add-classification", utilities.handleErrors(invCont.addClassification))
+router.post("/add-inventory", utilities.handleErrors(invCont.addInventory))
 
 module.exports = router
